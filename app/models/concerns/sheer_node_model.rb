17267f82ce8daba3f19c13ff908901784e2e4bfa
qinmingyuan <mingyuan0715@foxmail.com>
2018-07-19 12:21:36 +0800

    sheer ancestor_ids

--- app/models/concerns/sheer_node_model.rb
+++ app/models/concerns/sheer_node_model.rb
@@ -1,4 +1,4 @@
-module TheNodeModel
+module SheerNodeModel
   extend ActiveSupport::Concern
 
   included do
@@ -28,6 +28,27 @@ module TheNodeModel
       self.hierarchy_class.maximum(:generations).to_i + 1
     end
 
+  end
+
+  def sheer_descendant_ids(c_ids = child_ids)
+    @sheer_descendant_ids ||= c_ids.dup
+    get_ids = self.class.where(parent_id: c_ids).pluck(:id)
+    if get_ids.present?
+      _get_ids = get_ids - @sheer_descendant_ids
+      @sheer_descendant_ids.concat get_ids
+      sheer_descendant_ids(_get_ids)
+    else
+      @sheer_descendant_ids
+    end
+  end
+
+  def sheer_ancestor_ids
+    node, node_ids = self, []
+    until (node_ids + [self.id]).include?(node.parent_id)
+      node_ids << node.parent_id
+      node = node.parent
+    end
+    node_ids
   end
 
 end
