20526e13ba013912c9230e7ffa0e35dffe5ad907
qinmingyuan <mingyuan0715@foxmail.com>
2018-07-19 14:15:43 +0800

    remove module

--- app/models/concerns/the_node_model.rb
+++ app/models/concerns/the_node_model.rb
@@ -22,6 +22,27 @@ module TheNodeModel
     parent_id.present? && depth < self.class.max_depth
   end
 
+  def sheer_descendant_ids(c_ids = child_ids)
+    @sheer_descendant_ids ||= c_ids.dup
+    get_ids = self.class.where(parent_id: c_ids).pluck(:id)
+    if get_ids.present?
+      _get_ids = get_ids - @sheer_descendant_ids
+      @sheer_descendant_ids.concat get_ids
+      sheer_descendant_ids(_get_ids)
+    else
+      @sheer_descendant_ids
+    end
+  end
+
+  def sheer_ancestor_ids
+    node, node_ids = self, []
+    until (node_ids + [self.id]).include?(node.parent_id)
+      node_ids << node.parent_id
+      node = node.parent
+    end
+    node_ids
+  end
+
   module ClassMethods
 
     def max_depth
