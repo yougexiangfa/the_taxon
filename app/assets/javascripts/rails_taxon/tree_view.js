24b22780997a0c7349ad3433f231fe853ebe0b80
qinmingyuan <mingyuan0715@foxmail.com>
2018-11-11 20:39:56 +0800

    upgrade tree js

--- app/assets/javascripts/rails_taxon/tree_view.js
+++ app/assets/javascripts/rails_taxon/tree_view.js
@@ -1,14 +1,14 @@
 function collapse(el){
-  var par = $(el).parent().parent();
+  var par = el.parentNode.parentNode;
   var selector = 'tr[data-depth$="' + par.data('depth') + '"]';
   par.nextUntil(selector).hide();
   $(el).replaceWith('<i class="grey caret right icon link"></i>')
 }
 
 function expand(el){
-  var par = $(el).parent().parent()
+  var par = el.parentNode.parentNode;
   var selector = 'tr[data-depth$="' + par.data('depth') + '"]';
-  par.nextUntil(selector).show()
+  par.nextUntil(selector).show();
   $(el).replaceWith('<i class="grey caret down icon link"></i>')
 }
 
