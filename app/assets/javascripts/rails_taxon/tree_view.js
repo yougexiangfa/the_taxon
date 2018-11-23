ab344fa644249367fc53e4657b7240db62d71493
qinmingyuan <mingyuan0715@foxmail.com>
2018-11-23 23:32:27 +0800

    fix tree view js

--- app/assets/javascripts/rails_taxon/tree_view.js
+++ app/assets/javascripts/rails_taxon/tree_view.js
@@ -1,21 +1,31 @@
 function collapse(el){
   var par = el.parentNode.parentNode;
-  var selector = 'tr[data-depth$="' + par.data('depth') + '"]';
-  par.nextUntil(selector).hide();
-  $(el).replaceWith('<i class="grey caret right icon link"></i>')
+  var selector = 'tr[data-depth$="' + par.dataset['depth'] + '"]';
+  $(par).nextUntil(selector).hide();
+  el.classList.replace('down', 'right')
+  el.addEventListener('click', function(e){
+    expand(e.target)
+  })
 }
 
 function expand(el){
   var par = el.parentNode.parentNode;
-  var selector = 'tr[data-depth$="' + par.data('depth') + '"]';
-  par.nextUntil(selector).show();
-  $(el).replaceWith('<i class="grey caret down icon link"></i>')
+  var selector = 'tr[data-depth$="' + par.dataset['depth'] + '"]';
+  $(par).nextUntil(selector).show();
+  el.classList.replace('right', 'down')
+  el.addEventListener('click', function(e){
+    collapse(e.target)
+  })
 }
 
-document.querySelectorAll('.grey.caret.down.icon.link').addEventListener('click', function(e){
-  collapse(e.target);
-});
+document.querySelectorAll('i.grey.caret.down.icon.link').forEach(function(el) {
+  el.addEventListener('click', function(e){
+    collapse(e.target);
+  });
+})
 
-document.querySelectorAll('.grey.caret.right.icon.link').addEventListener('click', function(e){
-  expand(e.target);
-});
+document.querySelectorAll('i.grey.caret.right.icon.link').forEach(function(el) {
+  el.addEventListener('click', function(e){
+    expand(e.target);
+  });
+})
