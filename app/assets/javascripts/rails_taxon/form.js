8b81bf5457c6febe860182a046dd164f7f87dce1
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-01 15:09:15 +0800

    test children edit

--- app/assets/javascripts/rails_taxon/form.js
+++ app/assets/javascripts/rails_taxon/form.js
@@ -6,8 +6,10 @@ $('[data-title="parent_ancestors"]').dropdown({
       var search_url = new URL(window.location.origin + search_path);
       search_url.searchParams.set('node_id', value);
       search_url.searchParams.set('node_type', this.dataset['type']);
-      search_url.searchParams.set('index', this.dataset['index']);
-
+      search_url.searchParams.set('method', this.dataset['method']);
+      if (this.dataset['index']) {
+        search_url.searchParams.set('index', this.dataset['index']);
+      }
       Rails.ajax({url: search_url, type: 'get', dataType: 'script'});
     }
   }
