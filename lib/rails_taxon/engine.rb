62907b793a5a76cccae20612cb10d77556892353
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-26 10:53:14 +0800

    fix refactor issue

--- lib/rails_taxon/engine.rb
+++ lib/rails_taxon/engine.rb
@@ -1,10 +1,6 @@
 module RailsTaxon
   class Engine < ::Rails::Engine
 
-    config.autoload_paths += Dir[
-      "#{config.root}/app/models/rails_taxon"
-    ]
-
     initializer 'rails_taxon.assets.precompile' do |app|
       app.config.assets.precompile += ['rails_taxon_manifest.js']
     end
