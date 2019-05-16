8ee85bb7cab07a1bc4b0c7ec4836b19f4d18567d
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-16 11:43:28 +0800

    draw tree not fix variable name

--- app/helpers/rails_taxon_helper.rb
+++ app/helpers/rails_taxon_helper.rb
@@ -1,14 +1,17 @@
 module RailsTaxonHelper
 
-  def draw_tree(node, partial:)
+  # first locals must be node
+  def draw_tree(partial: , object: )
     str = ''
-    node.children.each do |child|
-      concat(render partial: partial, locals: { node: child })
+    
+    object.children.each do |child|
+      concat(render partial: partial, object: child)
 
       if child.children.any?
-        draw_tree(child, partial: partial)
+        draw_tree(partial: partial, object: child)
       end
     end
+   
     str
   end
 
