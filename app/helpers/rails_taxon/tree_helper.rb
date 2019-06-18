8c791277785bf2a299e13de2fc39f1b82c1ddebf
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-18 12:02:35 +0800

    support more options

--- app/helpers/rails_taxon/tree_helper.rb
+++ app/helpers/rails_taxon/tree_helper.rb
@@ -1,14 +1,14 @@
 module RailsTaxon::TreeHelper
 
   # first locals must be node
-  def draw_tree(partial: , object: )
+  def draw_tree(partial: , object: , **options)
     str = ''
     
     object.children.each do |child|
-      concat(render partial: partial, object: child)
+      concat(render partial: partial, object: child, **options)
 
       if child.children.any?
-        draw_tree(partial: partial, object: child)
+        draw_tree(partial: partial, object: child, **options)
       end
     end
    
