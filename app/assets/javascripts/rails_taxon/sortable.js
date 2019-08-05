cc22bd136441bccc22bbec62adecae937f787801
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-05 16:04:18 +0800

    add sortablejs

---
+++ app/assets/javascripts/rails_taxon/sortable.js
@@ -0,0 +1,25 @@
+//= require sortablejs
+
+class SortableController extends Stimulus.Controller {
+  connect() {
+    var el = document.getElementById('task_lists');
+    var sortable = Sortable.create(el, {
+      onEnd: function(evt) {
+        if (evt.oldIndex === evt.newIndex) {
+          return
+        }
+        var url = '/my/tasks/' + evt.item.getAttribute('data-id') + '/reorder';
+        var body = new FormData();
+        this.toArray().forEach(function(el){
+          body.append('sort_array[]', el);
+        });
+        body.append('old_index', evt.oldIndex);
+        body.append('new_index', evt.newIndex);
+
+        Rails.ajax({url: url, type: 'PATCH', dataType: 'script', data: body})
+      }
+    });
+  }
+}
+SortableController.targets = ['src'];
+application.register('sortable', SortableController);
