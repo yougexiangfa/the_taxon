b3bbf19cddab0d1b84d435e1193201d3dda19bbe
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-28 22:25:22 +0800

    moved db

--- app/models/rails_taxon/tag.rb
+++ app/models/rails_taxon/tag.rb
@@ -1,6 +1,10 @@
 module RailsTaxon::Tag
   extend ActiveSupport::Concern
   included do
+    attribute :name, :string
+    attribute :type, :string
+    attribute :taggeds_count, :integer, default: 0
+
     has_many :taggeds, dependent: :delete_all
   end
   
--- app/models/rails_taxon/taxon.rb
+++ app/models/rails_taxon/taxon.rb
@@ -1,6 +1,13 @@
 module RailsTaxon::Taxon
   extend ActiveSupport::Concern
   included do
+    attribute :name, :string
+    attribute :type, :string
+    attribute :color, :string
+    attribute :description, :string
+    attribute :position, :integer, default: 1
+    attribute :entities_count, :integer, default: 0
+
     has_closure_tree
     acts_as_list
     has_one_attached :cover
