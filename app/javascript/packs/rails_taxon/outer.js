2031c193eb91a55e21c69283ca9fc554ae7b714e
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-08 22:12:11 +0800

    drop semantic ui

--- app/javascript/packs/rails_taxon/outer.js
+++ app/javascript/packs/rails_taxon/outer.js
@@ -1,21 +1,20 @@
-$('[data-title="outer_ancestors"]').dropdown({
-  placeholder: false,
-  onChange: function(value, text, $selectedItem){
-    var search_path = '/nodes/outer'
-    var search_url = new URL(location.origin + search_path)
-    if (value) {
-      search_url.searchParams.set('node_id', value)
+document.querySelectorAll('select[data-title="outer_ancestors"]').forEach(function(el) {
+  el.addEventListener('change', function() {
+    let search_url = new URL(location.origin + '/nodes/outer')
+    if (this.value) {
+      search_url.searchParams.set('node_id', this.value)
       search_url.searchParams.set('node_type', this.dataset['nodeType'])
       search_url.searchParams.set('as', this.dataset['as'])
       search_url.searchParams.set('method', this.dataset['method'])
       search_url.searchParams.set('outer', this.dataset['outer'])
+      search_url.searchParams.set('html_id', this.id)
       if (this.dataset['index']) {
         search_url.searchParams.set('index', this.dataset['index'])
       }
 
       Rails.ajax({url: search_url, type: 'GET', dataType: 'script'})
     } else {
-      $(this).parent().parent().nextAll('[data-title="outer_ancestors_input"]').remove()
+      this.parentNode.parentNode.nextAll('[data-title="outer_ancestors_input"]').remove()
     }
-  }
-});
+  })
+})
